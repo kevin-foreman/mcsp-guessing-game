3374db5d00b0799aa1347a6f2355af1902545190
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-24 16:10:17 -0600

    refactored to record and output number of guesses

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 alert("This is the guessing game!");
-
 let guessGameBtn = document.querySelector("#guessGame");
-
 let randomNumber = Math.floor(Math.random() * 10 + 1);
-
-let guessGame = function() {
+let guessCount = 1;
+let guessGame = function () {
     let userGuess = prompt("Please guess a number between 0 and 10");
     console.log(randomNumber);
-    let guessCount = 0;
     // let userGuess = document.getElementById(guessGame).value;
     console.log(userGuess);
     if (userGuess >= 0 && userGuess <= 10) {
         if (userGuess == randomNumber) {
             alert("Congratulations, you guessed the random number " + userGuess)
-        } else {
-            alert("You did not successfully guess the random number " + randomNumber)
+            alert("It took you " + guessCount + " guesses to get the random number!");
+        } else if (userGuess > randomNumber) {
+            alert("Lower, please guess again");
+            guessCount += 1;
+            guessGame();
+        } else if (userGuess < randomNumber) {
+            alert("Higher, please guess again");
+            guessCount += 1;
+            guessGame();
         }
     }
 };
