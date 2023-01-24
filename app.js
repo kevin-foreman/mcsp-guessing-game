5dbce67c0929c097e989f71c102f298ed47e3711
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-24 16:32:59 -0600

    Updated to reload the page after the game to try again

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 alert("This is the guessing game!");
 let guessGameBtn = document.querySelector("#guessGame");
-let randomNumber = Math.floor(Math.random() * 10 + 1);
+let randomNumber = Math.floor(Math.random() * 100 + 1);
 let guessCount = 1;
 let guessGame = function () {
-    let userGuess = prompt("Please guess a number between 0 and 10");
+    let userGuess = prompt("Please guess a number between 0 and 100");
     console.log(randomNumber);
     // let userGuess = document.getElementById(guessGame).value;
     console.log(userGuess);
-    if (userGuess >= 0 && userGuess <= 10) {
+    if (userGuess >= 0 && userGuess <= 100) {
         if (userGuess == randomNumber) {
             alert("Congratulations, you guessed the random number " + userGuess)
             alert("It took you " + guessCount + " guesses to get the random number!");
+            alert("Click OK to try again!");
+            location.reload();
         } else if (userGuess > randomNumber) {
             alert("Lower, please guess again");
             guessCount += 1;
---
+++ app32.js
@@ -0,0 +1,36 @@
+let secret = getRandomInclusive(1, 100);
+
+function getRandomInclusive(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min) + 1);
+};
+
+function init() {
+    console.log("app.js started");
+    console.log(`secret number: ${secret}`);
+    let validInput = false;
+    while (!validInput) {
+        result = guessOnce();
+
+        if (result = guessOnce()) {
+            validInput = true;
+        }
+        alert(result);
+    }
+};
+
+function guessOnce() {
+    let numInp = prompt("Guess a number...");
+    // asssume user entered a valid integer
+    const num = parseInt(numInp);
+    if (num < secret) {
+        return "Lower"
+    } else if (num > secret) {
+
+    } else {
+        return "Correct"
+    }
+};
+
+init();
