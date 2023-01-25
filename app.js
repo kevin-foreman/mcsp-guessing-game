026f07765b22296ba4e1c3ccf6b5aa846978a481
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-25 11:51:49 -0600

    Updated to track guess history, just needs a tweak

--- app.js
+++ app.js
@@ -6,25 +6,30 @@ let guessGameBtn = document.querySelector("#guessGame");
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 // setting the count to 1 here, just in case the user gets the right number the first time
 let guessCount = 1;
+let guessesHistory = [];
 let guessGame = function () {
     let userGuess = prompt("Please guess a number between 0 and 100");
     // Use these console.logs to see the numbers and the information for testing
-    // console.log(randomNumber);
-    // console.log(userGuess);
-    // console.log(userName);
+    console.log(randomNumber);
+    console.log(userGuess);
+    console.log(userName);
+    
+    console.log(guessesHistory);
     if (userGuess >= 0 && userGuess <= 100) {
         if (userGuess == randomNumber) {
-            alert(`Congratulations, ${userName} you guessed the random number, ${userGuess}`)
-            alert("It took you " + guessCount + " guesses to get the random number!");
+            alert(`Congratulations, ${userName} you guessed the random number, ${randomNumber}`)
+            alert("It took you " + guessCount + " guesses to get the random number! Your guesses were " + guessesHistory);
             alert("Would you like to play again? If not, just close your browser.");
             location.reload();
         } else if (userGuess > randomNumber) {
             alert("Lower, please guess again");
             guessCount += 1;
+            guessesHistory.push(userGuess[1]);
             guessGame();
         } else if (userGuess < randomNumber) {
             alert("Higher, please guess again");
             guessCount += 1;
+            guessesHistory.push(userGuess[1]);
             guessGame();
         }
     };
